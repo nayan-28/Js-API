af46748cdecc368f9d3c3c682fb95f4191fd8255
nayan-28 <nayanmalakar720@gmail.com>
2024-02-01 12:48:53 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function loadUsers2() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => displayUser2(data))
+}
+
+function displayUser2(data) {
+    for (const user of data) {
+        console.log(user.name);
+    }
+}
